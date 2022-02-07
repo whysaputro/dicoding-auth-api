3bfd510cddf19ecc9c84e4a4ef525918dda8b9d8
Hendra Wahyu <hendrawahyu26@gmail.com>
2022-02-07 22:09:23 +0700

    refactory error handler server error

--- src/Infrastructures/http/CreateServer.js
+++ src/Infrastructures/http/CreateServer.js
@@ -1,4 +1,6 @@
 const Hapi = require('@hapi/hapi');
+const DomainErrorTranslator = require('../../Commons/exceptions/DomainErrorTranslator');
+const ClientError = require('../../Commons/exceptions/ClientError');
 const users = require('./api/users');
 
 const createServer = async (container) => {
@@ -13,6 +15,37 @@ const createServer = async (container) => {
       options: { container },
     },
   ]);
+
+  server.ext('onPreResponse', (request, h) => {
+    // mendapatkan konteks response dari request
+    const { response } = request;
+
+    if (response instanceof Error) {
+      // bila response tersebut error, tangani sesuai kebutuhan
+      const translatedError = DomainErrorTranslator.translate(response);
+
+      // penanganan client error secara internal.
+      if (translatedError instanceof ClientError) {
+        const newResponse = h.response({
+          status: 'fail',
+          message: translatedError.message,
+        });
+        newResponse.code(translatedError.statusCode);
+        return newResponse;
+      }
+
+      // penanganan server error sesuai kebutuhan
+      const newResponse = h.response({
+        status: 'error',
+        message: 'terjadi kegagalan pada server kami',
+      });
+      newResponse.code(500);
+      return newResponse;
+    }
+
+    // jika bukan error, lanjutkan dengan response sebelumnya (tanpa terintervensi)
+    return h.continue;
+  });
 
   return server;
 };
--- src/Infrastructures/http/api/users/handler.js
+++ src/Infrastructures/http/api/users/handler.js
@@ -1,6 +1,4 @@
 const AddUserUseCase = require('../../../../Applications/use_case/AddUserUseCase');
-const DomainErrorTranslator = require('../../../../Commons/exceptions/DomainErrorTranslator');
-const ClientError = require('../../../../Commons/exceptions/ClientError');
 
 class UsersHandler {
   constructor(container) {
@@ -10,39 +8,18 @@ class UsersHandler {
   }
 
   async postUserHandler(request, h) {
-    try {
-      const addUserUseCase = this._container.getInstance(AddUserUseCase.name);
-      const addedUser = await addUserUseCase.execute(request.payload);
+    const addUserUseCase = this._container.getInstance(AddUserUseCase.name);
+    const addedUser = await addUserUseCase.execute(request.payload);
 
-      const response = h.response({
-        status: 'success',
-        data: {
-          addedUser,
-        },
-      });
+    const response = h.response({
+      status: 'success',
+      data: {
+        addedUser,
+      },
+    });
 
-      response.code(201);
-      return response;
-    } catch (error) {
-      const translatedError = DomainErrorTranslator.translate(error);
-
-      if (translatedError instanceof ClientError) {
-        const response = h.response({
-          status: 'fail',
-          message: translatedError.message,
-        });
-
-        response.code(translatedError.statusCode);
-        return response;
-      }
-
-      const response = h.response({
-        status: 'error',
-        message: 'terjadi kegagalan pada server kami',
-      });
-      response.code(500);
-      return response;
-    }
+    response.code(201);
+    return response;
   }
 }
 
