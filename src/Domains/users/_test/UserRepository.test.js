dee4e8ced7963f6f6b8bbe77d04a7b24e14d2b45
Hendra Wahyu <hendrawahyu26@gmail.com>
2022-02-07 13:07:37 +0700

    create UserRepository Interface

---
+++ src/Domains/users/_test/UserRepository.test.js
@@ -0,0 +1,12 @@
+const UserRepository = require('../UserRepository');
+
+describe('UserRepository interface', () => {
+  it('should throw error when invoke abstract behaviour', async () => {
+    // Arrange
+    const userRepository = new UserRepository();
+
+    // Action and assert
+    await expect(userRepository.addUser({})).rejects.toThrowError('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    await expect(userRepository.verifyAvailableUsername('')).rejects.toThrowError('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  });
+});
